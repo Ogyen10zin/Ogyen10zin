703f940382dc559aa6f8fc3095cc5f8a24dd7ac3
Ogyen10zin <ogyen.ot@gmail.com>
2024-07-29 16:39:37 +1000

    Tutorial 2 upload
    Submission of tutorial week 2

---
+++ practice/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My page</title>
+</head>
+<body>
+<?php
+    $name = "Dr. Sai Lakkaraju";
+    echo "My favourite lecturer is " . $name;
+?>
+</body>
+</html>
